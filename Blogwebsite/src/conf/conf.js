c5e477625b1429117d552d279ddbc3727bd70ea9
Muhammad abid <abidniazi3443@gmail.com>
2026-02-26 01:39:22 +0500

    Basic setup for rnv

---
+++ Blogwebsite/src/conf/conf.js
@@ -0,0 +1,11 @@
+const conf={
+appWriteurl:String(import.meta.env.VITE_APP_WRITEURL),
+appwriteProjectId:String(import.meta.env.VITE_APP_PROJECT_ID),
+appwriteDatabaseId:String(import.meta.env.VITE_APP_DATABASE_ID),
+appwriteCollectionId:String(import.meta.env.VITE_APP_COLLECTION_ID),    
+appwriteBusketId:String(import.meta.env.VITE_APP_BUCKET_ID),
+
+}
+
+
+export default conf;

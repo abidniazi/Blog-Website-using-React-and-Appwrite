7a4d2b873a8232eb1521a47e4a2cd806e2805b10
Muhammad abid <abidniazi3443@gmail.com>
2026-02-26 02:22:56 +0500

    authentication file added

---
+++ Blogwebsite/src/appwrite/auth.js
@@ -0,0 +1,62 @@
+import { use } from 'react';
+import conf from '../conf/conf'
+import { Client, Account,ID } from 'appwrite'
+
+export class AuthService{
+ Client = new Client();
+ account;
+ 
+ Constructor(){
+    this.Client
+    .setEndpoint(conf.appWriteurl)
+    .setProject(conf.appwriteProjectId);
+    this.account= new Account(Client);
+ }
+ async createAccount({email,password,name}){
+    try {
+const userAccount= await this.account.create(ID.unique,email,password,name)
+if(userAccount){
+// call another method
+return this.login({email,password})
+}    else{
+    return userAccount
+    
+}
+
+} catch (error) {
+        console.log()
+    }
+ }
+
+ async login({email,password}){
+    try {
+        return await this.account.createEmailSession(email,password)
+
+    } catch (error) {
+        throw error
+    }
+ }
+
+ async getCurrentUser(){
+    try {
+        await this.account.get();
+        
+    } catch (error) {
+        console.log("appwrite getcurrent user:",error); 
+    }
+    return null;
+ }
+
+async logout(){
+    try {
+        await this.account.deleteSessions();
+    } catch (error) {
+        console.log("logOut:",error);
+        
+    }
+}
+
+}
+
+const authservice = new AuthService();
+export default authservice

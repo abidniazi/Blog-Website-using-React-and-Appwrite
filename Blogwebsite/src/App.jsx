46cd13311eb7b0a4a10afa3e5d3c1b26ce373e22
Muhammad abid <abidniazi3443@gmail.com>
2026-02-27 22:50:23 +0500

    Debugging

--- Blogwebsite/src/App.jsx
+++ Blogwebsite/src/App.jsx
@@ -1,16 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {useDispatch} from 'react-redux'
+import authservice from "./appwrite/auth.js"
+import {login,logout} from "./store/authSlice.js"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoding] = useState(true)
+  const dispatch=useDispatch();
 
-  return (
-  <>
-  <h1>Blog website</h1>
-  </>
-  )
+  useEffect(()=>{
+authservice.getCurrentUser()
+.then((userdata)=>{
+  if (userdata) {
+    dispatch(login({userdata}))
+  }else{
+    dispatch(logout())
+  }
+})
+.finally(setLoding(false))
+  },[])
+
+
+ return !loading ? (
+  <div>
+    Blog website
+  </div>
+ ) : null
 }
 
 export default App

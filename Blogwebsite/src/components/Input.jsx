c4fb4d0abe58a85fa5cf453df35af63b048d2afe
Muhammad abid <abidniazi3443@gmail.com>
2026-03-03 00:02:43 +0500

    input field setups

--- Blogwebsite/src/components/Input.jsx
+++ Blogwebsite/src/components/Input.jsx
@@ -1,7 +1,30 @@
+import { ID } from "appwrite";
 import React,{useId} from "react";
 
-const Input=React.forwardRef(function Input(){
-    return <h1>test</h1>
+const Input=React.forwardRef(function Input(
+    
+    {
+        label,
+        type="text",
+        className="",
+        id,
+        ...props
+    },ref){
+    return (
+        <div className="w-full">
+        {
+            label && <label className="inline-block mb-1 pl-1" htmlFor={id}></label>
+        }
+        <input
+        type={type}
+        className={`w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600 ${className}`}
+        ref={ref}
+        {...props}
+        id={id}
+        >
+        </input>
+        </div>
+    )
 })
 
 export default Input;

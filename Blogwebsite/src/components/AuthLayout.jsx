a916c34a6cfca8f01349ae1de10d82f3b084440e
Muhammad abid <abidniazi3443@gmail.com>
2026-03-05 04:15:25 +0500

    auth layout

---
+++ Blogwebsite/src/components/AuthLayout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AuthLayout({ children,authentication=true }) {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            {children}
+        </div>
+    );
+}
+
+export default AuthLayout;

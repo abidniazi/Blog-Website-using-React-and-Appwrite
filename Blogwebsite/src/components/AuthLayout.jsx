25605d484b0ca82760e46da203304327640cfc31
Muhammad abid <abidniazi3443@gmail.com>
2026-03-05 04:17:56 +0500

    ..

--- Blogwebsite/src/components/AuthLayout.jsx
+++ Blogwebsite/src/components/AuthLayout.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
+import {useSelector} from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 
 function AuthLayout({ children,authentication=true }) {
+    const naviagate = useNavigate();
+    const [loader,setLoader] = useState(true);
+    const authStatue=useSelector(state=>state.authStatue);
+    
+    
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
             {children}

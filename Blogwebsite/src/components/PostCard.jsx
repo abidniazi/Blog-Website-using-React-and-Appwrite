5739cc278a918322d435d2c3042be81ca3a11be7
Muhammad abid <abidniazi3443@gmail.com>
2026-03-05 03:37:39 +0500

    posts card components building

---
+++ Blogwebsite/src/components/PostCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import appWriteService from "../appwrite/config";
+import { Link } from "react-router-dom";
+
+
+function PostCard({$id,title,featuredImage}){
+    return(
+        <link to={`/post/${$id}`}>
+<div className="w-full bg-gray-100 rounded-xl p-4">
+<div className="w-full justify-center mb-4">
+<img src={appWriteService. gefilePreview(featuredImage)} alt={title}/>
+</div>
+<h2 className="text-xl font-blod">{title} </h2>
+</div>
+        </link>
+    )
+}
+
+export default PostCard;
